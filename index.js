b2baca75a0aff0952aa89c3413d0d5fe4588ca5f
Chevol Davis <chevol@chevol.com>
2018-10-25 09:28:49 -0400

    Initial check in

---
+++ index.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//PUBLIC Files
+app.get('/chat', function(req, res){
+  res.sendFile(__dirname + '/public/');
+});
+app.get('/chat.css', function(req, res){
+  res.sendFile(__dirname + '/public/chat.css');
+});
+
+//SOCKET.IO INFO
+io.on('connection', function(socket){
+  console.log('User Connected');
+
+  socket.on('chat message', function(msgObj){
+   console.log(msgObj);
+   io.emit('chat message', msgObj);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('User Disconnected');
+  });
+
+});
+
+//Start Listening for requests
+http.listen(3000, function(){
+  console.log('listening on port 3000');
+});
